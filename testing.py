f0829f26a0126717d20db0793296bb615ef133cc
Python Hacker <40a5h0+epr78unl7xyvk@sharklasers.com>
2016-06-15 12:24:59 +0530

    Create testing.py

---
+++ testing.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python
+print "This is for Just testing on Repositories contributed section! "
+print "I hope you don't get angry over the test as name of your repo is bogus "
+print "Sayonara"
